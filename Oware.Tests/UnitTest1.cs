a2eb1c27c495b44894f62a0964f10803aadccdad
Thabiso <g19m6062@campus.ru.ac.za>
2021-05-16 18:59:12 +0200

    Testing complete - 2nd commit

--- Oware.Tests/UnitTest1.cs
+++ Oware.Tests/UnitTest1.cs
@@ -3,7 +3,24 @@ using NUnit.Framework;
 namespace Oware.Tests
 {
     public class Tests
-    {
+    {   
+        public class MockScoreHouse : IScoreHouse
+        {   
+            public void AddSeed(Seed seed)
+            {
+            }
+
+            public int GetCount()
+            {
+                return 1; //throw new System.NotImplementedException();
+            }
+
+            public void Reset()
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
         [SetUp]
         public void Setup()
         {
@@ -12,7 +29,30 @@ namespace Oware.Tests
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            //ARRANGE
+            House h = new House(0, 0);
+            Seed seed = new Seed();
+            h.AddSeedInPot(seed);
+            //ACT
+            h.ResetHouse(); 
+            //ASSERT
+            Assert.AreEqual(4, h.GetCount(), "Resetting the house should restore the House to 4 seeds in the pot");
+        }
+
+        [Test]
+        public void Test2()
+        {   
+            //ARRANGE
+            MockScoreHouse oneScore = new MockScoreHouse();
+            MockScoreHouse twoScore = new MockScoreHouse();
+            Player one = new Player("Player 1", oneScore);
+            Player two = new Player("Player 2", twoScore);
+            Board b = new Board(one, two);
+            //ACT          
+            Seed seed = new Seed();
+            one.AddSeedToScoreHouse(seed);
+            //ASSERT
+            Assert.AreEqual(1,one.GetScore(),"The score should be one");
         }
     }
 }
--- Oware/Program.cs
+++ Oware/Program.cs
@@ -7,9 +7,11 @@ namespace Oware
     class Program
     {
         static void Main(string[] args)
-        {
-            Player one = new Player("Player 1");
-            Player two = new Player("Player 2");
+        {   
+            IScoreHouse oneScore = new ScoreHouse();
+            IScoreHouse twoScore = new ScoreHouse();
+            Player one = new Player("Player 1", oneScore);
+            Player two = new Player("Player 2", twoScore);
             Board b = new Board(one, two);
             // rest left as exercise to reader!
         }
